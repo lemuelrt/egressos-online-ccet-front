b2973c24d2b93512bcc87f92c900de346296d4e7
Eliézer De Oliveira <eliezer.oliveirasiueg@gmail.com>
2018-03-22 15:53:10 -0300

    add Link para cadastro de egresso(s) e botão para importar arquivo csv

--- src/app/components/coord/coord-egresso-index/coord-egresso-index.component.ts
+++ src/app/components/coord/coord-egresso-index/coord-egresso-index.component.ts
@@ -10,6 +10,17 @@ export class CoordEgressoIndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+
+      // tslint:disable-next-line:prefer-const
+      let fileSelect = document.getElementById('fileSelect'),
+        // tslint:disable-next-line:prefer-const
+        fileElem = document.getElementById('fileElem');
+
+      fileSelect.addEventListener('click', function (e) {
+        if (fileElem) {
+          fileElem.click();
+        }
+      }, false);
+    }
 
 }
