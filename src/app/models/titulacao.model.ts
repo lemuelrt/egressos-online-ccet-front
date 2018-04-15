9436c2e39b69245c8b692bc60d1e9e21d4ddfc73
Danilo Nogueira <danilonogsi@gmail.com>
2018-04-15 10:04:02 -0300

    Troca de nome das tabelas. Add atributos.

--- src/app/models/titulacao.model.ts
+++ src/app/models/titulacao.model.ts
@@ -1,12 +1,4 @@
-import { TipoFormacao } from './tipo-formacao.model';
-import { Egresso } from './egresso.model';
-
-
 export interface Titulacao {
-
   titulacaoId: string;
-  tipoFormacao: TipoFormacao;
-  egresso: Egresso;
-  titulacaoAnoConclusao: number;
-  titulacaoArea: string;
+  titulacaoNome: string;
 }
