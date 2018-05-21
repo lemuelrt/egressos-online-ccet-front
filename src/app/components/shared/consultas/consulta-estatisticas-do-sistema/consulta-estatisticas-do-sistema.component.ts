0c3799f7c9750b031bc4c916849b20ec9c24a8ac
Eliézer De Oliveira <eliezer.oliveirasiueg@gmail.com>
2018-05-21 12:11:49 -0300

    Criação do componentde estatísticas do sistema

---
+++ src/app/components/shared/consultas/consulta-estatisticas-do-sistema/consulta-estatisticas-do-sistema.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-estatisticas-do-sistema',
+  templateUrl: './consulta-estatisticas-do-sistema.component.html',
+  styleUrls: ['./consulta-estatisticas-do-sistema.component.css']
+})
+export class ConsultaEstatisticasDoSistemaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
